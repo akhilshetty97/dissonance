aa461af63e95988e921f183a6e47f54af1bc1a44
Akhil Shetty <47709446+akhilshetty97@users.noreply.github.com>
2024-04-17 14:51:06 -0400

    Add video

--- thesis-webpage/src/Home.jsx
+++ thesis-webpage/src/Home.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './Home.css'
 import Line from './Line.jsx'
+import staticVideo from './assets/STATIC_SCREENSAVER.mp4'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-      <div className='bg-black w-full h-screen'>
+      <div className='w-full h-screen'>
+        <video className='w-full h-full object-cover' src={staticVideo} autoPlay loop muted />
+        <div className='absolute w-full h-100 top-0'>
           <div className='font-degular-bold text-white text-4xl pt-20 text-center'>DISSONANCE</div> 
           <div className='flex justify-around items-center' style={{ paddingTop: '100px' }}>
             <p className='font-input-mono text-white text-xs'>CONSISTENCY</p>
@@ -17,6 +20,7 @@ function App() {
             <Line/>
             <p className='font-input-mono text-white text-xs'>ADAPTATION</p>
           </div> 
+        </div>
       </div>
   )
 }
