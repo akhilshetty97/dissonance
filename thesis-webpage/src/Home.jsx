cdb11847afcafb8d1ea010414be3e74f7b372254
Akhil Shetty <47709446+akhilshetty97@users.noreply.github.com>
2024-04-25 17:57:04 -0400

    final

--- thesis-webpage/src/Home.jsx
+++ thesis-webpage/src/Home.jsx
@@ -37,22 +37,22 @@ function App() {
         <div className='absolute inset-0 flex flex-col justify-center items-center'>
           <div className='font-degular-bold text-white text-5xl text-center cursor-default absolute top-0 pt-20 2xl:pt-28'>DISSONANCE</div> 
           <div className='flex items-center pt-8 2xl:pt-12'> 
-            <div className='flex-col items-center px-12 2xl:px-20' onMouseEnter={() => handleVideoChange('1')} onMouseLeave={() => setShowTextArea(false)}>
+            <div className='flex-col items-center px-12 2xl:px-40' onMouseEnter={() => handleVideoChange('1')} onMouseLeave={() => setShowTextArea(false)}>
               <p className={`text-white text-sm text-center ${showTextArea==='1' ? 'font-input-mono-bold cursor-default':'font-input-mono'}`}>CONSISTENCY</p>
               <TextArea line1={`//a low hum;`} line2={`An initial sense of <br/>order and equilibrium <br/>establishes a mental <br/> comfort zone.`} id={'1'} show={showTextArea}/>
             </div>
             <Line/>
-            <div className='flex-col items-center px-12 2xl:px-20' onMouseEnter={() => handleVideoChange('2')} onMouseLeave={() => setShowTextArea(false)}>
+            <div className='flex-col items-center px-12 2xl:px-40' onMouseEnter={() => handleVideoChange('2')} onMouseLeave={() => setShowTextArea(false)}>
               <p className={`text-white text-sm text-center ${showTextArea==='2' ? 'font-input-mono-bold cursor-default':'font-input-mono'}`}>CONFLICT</p>
               <TextArea line1={`//the crescendo`} line2={`Jarring contradictory<br/>stimuli intrude, <br/>generating profound<br/>tension and unease.`} id={'2'} show={showTextArea}/>
             </div>
             <Line/>
-            <div className='flex-col items-center px-12 2xl:px-20' onMouseEnter={() => handleVideoChange('3')} onMouseLeave={() => setShowTextArea(false)}>
+            <div className='flex-col items-center px-12 2xl:px-40' onMouseEnter={() => handleVideoChange('3')} onMouseLeave={() => setShowTextArea(false)}>
               <p className={`text-white text-sm text-center ${showTextArea==='3' ? 'font-input-mono-bold cursor-default':'font-input-mono'}`}>TURMOIL</p>
               <TextArea line1={`//the unraveling`} line2={`Opposing perspectives<br/>intensify into<br/>sensory overload and<br/>mental chaos.`} id={'3'} show={showTextArea}/>
             </div>
             <Line/>
-            <div className='flex-col items-center px-12 2xl:px-20' onMouseEnter={() => handleVideoChange('4')} onMouseLeave={() => setShowTextArea(false)}>
+            <div className='flex-col items-center px-12 2xl:px-40' onMouseEnter={() => handleVideoChange('4')} onMouseLeave={() => setShowTextArea(false)}>
               <p className={`text-white text-sm text-center ${showTextArea==='4' ? 'font-input-mono-bold cursor-default':'font-input-mono'}`}>ADAPTATION</p>
               <TextArea line1={`//reconciliation;`} line2={`A fragile resolution<br/>emerges from the<br/>upheaval, allowing new<br/>synthesis to take shape.`} id={'4'} show={showTextArea}/>
             </div>
